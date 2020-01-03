e321a0136444a55a8ea49ee17624e9f9ec111fdc
Jacob Miller <jacobmiller22@vt.edu>
2020-01-03 12:07:54 -0500

    Initial Commit

---
+++ WaterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterApp
+//
+//  Created by Jacob Miller on 1/3/20.
+//  Copyright © 2020 Jacob Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
